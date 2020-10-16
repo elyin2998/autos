85eca6b53650bbf9bc627fdbbfa1c13168521fb3
Itihell Mejia <itihell.mejia@gmail.com>
2020-10-15 23:03:35 -0600

    agregando laravel collective

--- resources/views/resultados/suma.blade.php
+++ resources/views/resultados/suma.blade.php
@@ -1,5 +1,10 @@
 @extends('plantilla')
 @section('menu', View::make('navegacion.menu2'))
 @section('contenido')
-<h1>La suma es  {{ $suma }}</h1>
+<h1>La suma es {{ $suma }}</h1>
+
+{!! Form::open(['url' => 'foo/bar']) !!}
+{{ Form::label('Hola', null, ['class' => 'control-label']) }}
+{{ Form::text('Holame', "valor ", array_merge(['class' => 'form-control'], ['color'=>'red'])) }}
+{!! Form::close() !!}
 @endsection
